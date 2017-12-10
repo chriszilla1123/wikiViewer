6c71aed6cf9ade531f2829a0d16b92ba6f39aec5
Chris Hill <chriszilla1123@gmail.com>
2017-12-10 13:48:52 -0800

    made entire search entry clickable

--- script/script.js
+++ script/script.js
@@ -2,32 +2,37 @@
   var numSearch = 10
   var baseURL = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&origin=*&limit="
       + numSearch + "&search="
-  var userSearch = "google"
-  var callApi = baseURL + userSearch;
+  var userSearch;
   
- window.onkeyup = function(e){
-   var key = e.keyCode;
+  window.onkeyup = function(e){
+    var key = e.keyCode;
    
-   if (key == 13){
-     search(callApi);
-   }
+    if (key == 13 && document.getElementById("input").value.length != 0){
+      userSearch = encodeURI(document.getElementById("input").value);
+      var callApi = baseURL + userSearch;
+      search(callApi);
+    }
  }  
  
- function search(url){
-   console.log(url);
-   var xhttp = new XMLHttpRequest();
-   xhttp.onreadystatechange = function() {
-     if(this.readyState == 4){
-       var json = JSON.parse(this.response);
-       for(i=0; i < numSearch; i++){
-          document.getElementById("result" + i).innerHTML = ("<h3>" + json[1][i] + "</h3>"
-              + "<p>" + json[2][i] + "</p>"
-              + "<a href='" + json[3][i] + "'>Link</a>");
-       }
-     }
-   };
-   xhttp.open("GET", url, true);
-   xhttp.send();
- }
- 
+  function search(url){
+    console.log(url);
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if(this.readyState == 4){
+        var json = JSON.parse(this.response);
+        for(i=0; i < numSearch; i++){
+          document.getElementById('result' + i).setAttribute('href', json[3][i]);
+          document.getElementById('result' + i).setAttribute('target', '_blank');
+          document.getElementById('result' + i).innerHTML = (
+              '<div class="result">'
+              + '<h3>' + json[1][i] + '</h3>'
+              + '<p>' + json[2][i] + '</p>'
+              + '</div>'
+              );
+        }
+      }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+  }
 })();
